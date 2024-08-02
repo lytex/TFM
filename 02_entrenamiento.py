f2c1d9d80505d54839faeb72a5e47c95286a44b3
lytex <julianlpc15@gmail.com>
2024-08-02 20:43:07 +0200

    callback para escoger el mejor modelo

--- 02_entrenamiento.py
+++ 02_entrenamiento.py
@@ -62,7 +62,7 @@ def load_files(file, path):
 
 func = partial(load_files, path=path)
 
-lightcurves = progress_map(func, files[10:], n_cpu=64, total=len(files), executor='processes', error_behavior='raise')
+lightcurves = progress_map(func, files, n_cpu=64, total=len(files), executor='processes', error_behavior='raise')
 
 # for file in tqdm(files):
 #     lightcurves.append(func(file))
@@ -398,6 +398,74 @@ class F1_Score(tf.keras.metrics.Metric):
 
 
 # %%
+from keras.callbacks import Callback
+class GetBest(Callback):
+    """Get the best model at the end of training.
+	# Arguments
+        monitor: quantity to monitor.
+        verbose: verbosity mode, 0 or 1.
+        mode: one of {auto, min, max}.
+            The decision
+            to overwrite the current stored weights is made
+            based on either the maximization or the
+            minimization of the monitored quantity. For `val_acc`,
+            this should be `max`, for `val_loss` this should
+            be `min`, etc. In `auto` mode, the direction is
+            automatically inferred from the name of the monitored quantity.
+        period: Interval (number of epochs) between checkpoints.
+	# Example
+		callbacks = [GetBest(monitor='val_acc', verbose=1, mode='max')]
+		mode.fit(X, y, validation_data=(X_eval, Y_eval),
+                 callbacks=callbacks)
+    """
+
+    def __init__(self, monitor='val_loss', verbose=0,
+                 mode='auto', period=1):
+        super(GetBest, self).__init__()
+        self.monitor = monitor
+        self.verbose = verbose
+        self.period = period
+        self.best_epochs = 0
+        self.epochs_since_last_save = 0
+
+        if mode not in ['auto', 'min', 'max']:
+            warnings.warn('GetBest mode %s is unknown, '
+                          'fallback to auto mode.' % (mode),
+                          RuntimeWarning)
+            mode = 'auto'
+
+        if mode == 'min':
+            self.monitor_op = np.less
+            self.best = np.Inf
+        elif mode == 'max':
+            self.monitor_op = np.greater
+            self.best = -np.Inf
+        else:
+            if 'acc' in self.monitor or self.monitor.startswith('fmeasure'):
+                self.monitor_op = np.greater
+                self.best = -np.Inf
+            else:
+                self.monitor_op = np.less
+                self.best = np.Inf
+                
+    def on_train_begin(self, logs=None):
+        self.best_weights = self.model.get_weights()
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        current = logs.get(self.monitor)
+        if self.monitor_op(current, self.best):
+            self.best = current
+            self.best_weights = self.model.get_weights()
+                    
+    def on_train_end(self, logs=None):
+        if self.verbose > 0:
+            print('Using epoch %05d with %s: %0.5f' % (self.best_epochs, self.monitor,
+                                                       self.best))
+        self.model.set_weights(self.best_weights)
+
+
+# %%
 inputs, _, X_entire, _, y_entire, _, _, kepid_train, num2class, \
     output_classes = get_data_split(lightcurves, binary_classification=binary_classification, use_wavelet=use_wavelet, test_size=1.0)
 
@@ -417,7 +485,10 @@ if globals().get("model_1"):
 # device.reset()
 
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-cp_callback = tf.keras.callbacks.BackupAndRestore(log_dir)
+cp_callback = tf.keras.callbacks.ModelCheckpoint(log_dir, monitor='val_loss', save_best_only=True)
+best_callback = GetBest(monitor='val_loss', verbose=0, mode='min')
+# cp_callback = tf.keras.callbacks.BackupAndRestore(log_dir)
+
 
 if use_wavelet:
     lightcurves_filtered = sorted(lightcurves, key=lambda lc: lc.headers["id"])
@@ -452,21 +523,20 @@ if use_wavelet:
                         metrics=[F1_Score(),])
 
 else:
-    model_1.compile(loss = 'binary_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=1e-7,),
+    model_1.compile(loss = 'binary_crossentropy', optimizer=tf.keras.optimizers.Adam(),
                     metrics=['accuracy', tf.keras.metrics.Recall(), tf.keras.metrics.Precision(),])
 
 tf.keras.utils.plot_model(model_1, "model.png")
 tf.keras.utils.model_to_dot(model_1).write("model.dot")
 
 # %%
-
 history_1 =  pd.DataFrame()
 
 if k_fold is None:
     inputs, X_train, X_test, y_train, y_test, y, kepid_test, kepid_train, num2class, \
         output_classes = get_data_split(lightcurves, binary_classification=binary_classification, use_wavelet=use_wavelet)
     temp = model_1.fit(X_train, y_train, epochs=10, batch_size=128, validation_data=(X_test, y_test),
-                            callbacks=[cp_callback])
+                            callbacks=[cp_callback, best_callback])
     history_1 = history_1.append(pd.DataFrame(temp.history))
 
 else:
@@ -478,8 +548,8 @@ else:
             output_classes = get_data_split(lightcurves_kfold, binary_classification=binary_classification, use_wavelet=use_wavelet)
         inputs, X_train, X_test_kfold, y_train, y_test_kfold, y, kepid_test_kfold, kepid_train, num2class, \
             output_classes = get_data_split(lightcurves_kfold, binary_classification=binary_classification, use_wavelet=use_wavelet, k_fold=k_fold, ind=ind)
-        temp = model_1.fit(X_train, y_train, epochs=10, batch_size=128, validation_data=(X_test, y_test),
-                                callbacks=[cp_callback])
+        temp = model_1.fit(X_train, y_train, epochs=30, batch_size=128, validation_data=(X_test, y_test),
+                                callbacks=[best_callback])
         history_1 = history_1.append(pd.DataFrame(temp.history))
 
 history_1 = history_1.reset_index().rename(columns={"index": "epoch"})
@@ -531,8 +601,6 @@ else:
     plt.show()
 
 # %%
-
-# %%
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 num2class_vec = np.vectorize(num2class.get)
@@ -547,6 +615,13 @@ else:
 
 cm = confusion_matrix(num2class_vec(y_test_sampled), num2class_vec(y_predict_sampled), labels=[str(v) for v in num2class.values()])
 ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[str(v) for v in num2class.values()]).plot(xticks_rotation='vertical')
+
+precision = cm[0][0]/(cm[0][0] + cm[1][0])
+recall = cm[0][0]/(cm[0][0] + cm[0][1])
+F1 = 2*(precision*recall)/(precision+recall)
+β = 1
+Fβ = (1+β**2)*(precision*recall)/(β**2*precision+recall)
+print("P : %f\nR : %f\nF1: %f\nFβ: %f" % (precision, recall, F1, Fβ))
 
 # %%
 0/0
