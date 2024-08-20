b97f5afb6c267a047bf279325b8a3935506897bf
lytex <julianlpc15@gmail.com>
2024-08-21 01:58:07 +0200

    ovh username

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fallocate -l 400G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-exec python3.7 optuna_trial.py
+fallocate -l 400G /home/swapfile
+chmod 600 /home/swapfile
+mkswap /home/swapfile
+swapon /home/swapfile
+su - ovh -c 'cd /home/ovh/code && exec python3.7 optuna_trial.py'
