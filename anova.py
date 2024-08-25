b213e90f9803ab5e90ea816181c80680a8512093
lytex <julianlpc15@gmail.com>
2024-08-25 13:36:01 +0200

    optuna trial con binary classification

--- anova.py
+++ anova.py
@@ -17,16 +17,29 @@
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
 import pandas as pd
-df = pd.read_csv('all.csv')
+numeric_level_list = True
+df = pd.read_csv('all_data_2024-07-17/20240819-190659/all.csv')
 df['global_level_list'] = df['global_level_list'].apply(eval)
 df['local_level_list'] = df['local_level_list'].apply(eval)
+if numeric_level_list:
+    df['global_level_list'] = df['global_level_list'].apply(lambda row: sum([2**i for i in row]))
+    df['local_level_list'] = df['local_level_list'].apply(lambda row: sum([2**i for i in row]))
 df_red = df.loc[:, (df != df.iloc[0]).any()][['use_wavelet',  'global_level_list', 'local_level_list', 'l1',
        'l2', 'dropout', 'frac',  'F1', 'F1_val', ]]
 
-lm = ols('F1_val ~ C(use_wavelet) + C(global_level_list) + C(local_level_list) + l1 + l2 + dropout + frac',
-         data=df_red).fit()
-table = sm.stats.anova_lm(lm, typ=3, robust="hc3") # Type 2 ANOVA DataFrame
+if numeric_level_list:
+    lm = ols('F1_val ~ global_level_list + local_level_list + l1 + l2 + dropout + frac',
+             data=df_red).fit()
+else:
+    lm = ols('F1_val ~  C(global_level_list) + C(local_level_list) + l1 + l2 + dropout + frac',
+             data=df_red).fit()
+table = sm.stats.anova_lm(lm, typ=2, robust="hc3") # Type 2 ANOVA DataFrame
+print(lm.summary())
+print(table.to_latex())
 table
+
+# %%
+df['global_level_list'] 
 
 # %%
 lm.params.sort_values(ascending=False)
@@ -46,15 +59,51 @@ df_red_oh = df_red_oh.join(pd.DataFrame(mlb.fit_transform(df_red_oh.pop('local_l
                           index=df_red_oh.index))
 df_red_oh
 
+categorical = False
+if categorical:
+    lm = ols(f'F1_val ~ {"+".join([f"C({k})" for k in df_red_oh.keys() if k.startswith("global")])} + \
+        {"+".join([f"C({k})" for k in df_red_oh.keys() if k.startswith("local")])}',
+             data=df_red_oh).fit()
 
-lm = ols(f'F1_val ~ C(use_wavelet) + {"+".join([f"C({k})" for k in df_red_oh.keys() if k.startswith("global")])} + \
-    {"+".join([f"C({k})" for k in df_red_oh.keys() if k.startswith("local")])} + l1 + l2 + dropout + frac',
-         data=df_red_oh).fit()
-
-lm = ols(f'F1_val ~ use_wavelet + {"+".join([f"{k}" for k in df_red_oh.keys() if k.startswith("global")])} + \
-    {"+".join([f"{k}" for k in df_red_oh.keys() if k.startswith("local")])} + l1 + l2 + dropout + frac',
-         data=df_red_oh).fit()
-table = sm.stats.anova_lm(lm, typ=3, robust="hc3") # Type 2 ANOVA DataFrame
+else:
+    lm = ols(f'F1_val ~ {"+".join([f"{k}" for k in df_red_oh.keys() if k.startswith("global")])} + \
+        {"+".join([f"{k}" for k in df_red_oh.keys() if k.startswith("local")])}',
+             data=df_red_oh).fit()
+table = sm.stats.anova_lm(lm, typ=2, robust="hc3") # Type 2 ANOVA DataFrame
+print(lm.summary())
 table
 
 
+
+# %%
+df = pd.read_csv('all_data_2024-07-17/20240819-190659/all.csv')
+print("\n".join([f"{k} = {v if k != 'wavelet_family' and not pd.isna(v) else repr(v) if k =='wavelet_family' else 'None'}" for k, v in df.sort_values(by="F1_val", ascending=False).iloc[0, 0:22].items()]))
+
+# %%
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+import pandas as pd
+numeric_level_list = True
+df = pd.read_csv('all_data_2024-07-17/20240819-190659/all.csv')
+df['global_level_list'] = df['global_level_list'].apply(eval)
+df['local_level_list'] = df['local_level_list'].apply(eval)
+if numeric_level_list:
+    df['global_level_list'] = df['global_level_list'].apply(lambda row: sum([2**i for i in row]))
+    df['local_level_list'] = df['local_level_list'].apply(lambda row: sum([2**i for i in row]))
+df_red = df.loc[:, (df != df.iloc[0]).any()][['use_wavelet',  'global_level_list', 'local_level_list', 'l1',
+       'l2', 'dropout', 'frac',  'F1', 'F1_val', ]]
+
+if numeric_level_list:
+    lm = ols('F1_val ~ global_level_list + local_level_list + l1 + l2 + dropout + frac',
+             data=df_red).fit()
+else:
+    lm = ols('F1_val ~ C(use_wavelet) + C(global_level_list) + C(local_level_list) + l1 + l2 + dropout + frac',
+             data=df_red).fit()
+table = sm.stats.anova_lm(lm, typ=2, robust="hc3") # Type 2 ANOVA DataFrame
+table
+
+# %%
+from scipy.stats import f_oneway
+df = pd.read_csv('all_data_2024-07-17/20240819-190659/all.csv')
+f_oneway(df.query("use_wavelet").F1_val.dropna().to_numpy(), df.query("~use_wavelet").F1_val.dropna().to_numpy())
+df.query("use_wavelet").F1_val.dropna().describe(), df.query("~use_wavelet").F1_val.dropna().describe()
--- optuna_trial.py
+++ optuna_trial.py
@@ -68,7 +68,8 @@ def objective(trial, global_level_list=None, local_level_list=None, use_wavelet=
     global lightcurves_no_wavelet
 
 
-    binary_classification = trial.suggest_categorical("binary_classification", [True, False])
+    # binary_classification = trial.suggest_categorical("binary_classification", [True, False])
+    binary_classification = True
     use_wavelet = use_wavelet or not trial.params["binary_classification"]
     levels_global = 6
     levels_local = 3
@@ -119,43 +120,26 @@ def objective(trial, global_level_list=None, local_level_list=None, use_wavelet=
     n_proc = int(multiprocessing.cpu_count()*1.25)
     parallel = True
 
-    try:
-        precision, recall, F1, Fβ, cm, num2class, precision_val, recall_val, F1_val, Fβ_val, cm_val, history_1 = main(sigma=sigma, sigma_upper=sigma_upper,
-                    num_bins_global=num_bins_global, bin_width_factor_global=bin_width_factor_global,
-                    num_bins_local=num_bins_local, bin_width_factor_local=bin_width_factor_local, num_durations=num_durations,
-                    levels_global=levels_global, levels_local=levels_local, wavelet_family=wavelet_family,
-                    use_wavelet=use_wavelet, binary_classification=binary_classification,
-                    k_fold=k_fold,
-                    global_level_list=global_level_list, local_level_list=local_level_list,
-                    l1=l1, l2=l2, dropout=dropout,
-                    epochs=epochs, batch_size=batch_size,
-                    frac=frac, β=β,
-                    download_dir=download_dir,
-                    path=path,
-                    df_path=df_path,
-                    use_download_cache=use_download_cache,
-                    n_proc=n_proc,
-                    parallel=parallel,
-                    lightcurve_cache=lightcurve_cache,
-                    lightcurves=lightcurves,
-            )
-    except Exception as exc:
-
-        print("optuna exc:", exc)
-        import traceback
-        traceback.print_tb(exc.__traceback__)
-        precision = 0
-        recall = 0
-        F1 = 0
-        Fβ = 0
-        cm = [[0, 0], [0, 0]]
-        num2class = {0: None, 1: None}
-        precision_val = 0
-        recall_val = 0
-        F1_val = 0
-        Fβ_val = 0
-        cm_val = [[0, 0], [0, 0]]
-        history_1 = []
+    precision, recall, F1, Fβ, cm, num2class, precision_val, recall_val, F1_val, Fβ_val, cm_val, history_1 = main(sigma=sigma, sigma_upper=sigma_upper,
+                num_bins_global=num_bins_global, bin_width_factor_global=bin_width_factor_global,
+                num_bins_local=num_bins_local, bin_width_factor_local=bin_width_factor_local, num_durations=num_durations,
+                levels_global=levels_global, levels_local=levels_local, wavelet_family=wavelet_family,
+                use_wavelet=use_wavelet, binary_classification=binary_classification,
+                k_fold=k_fold,
+                global_level_list=global_level_list, local_level_list=local_level_list,
+                l1=l1, l2=l2, dropout=dropout,
+                epochs=epochs, batch_size=batch_size,
+                frac=frac, β=β,
+                download_dir=download_dir,
+                path=path,
+                df_path=df_path,
+                use_download_cache=use_download_cache,
+                n_proc=n_proc,
+                parallel=parallel,
+                lightcurve_cache=lightcurve_cache,
+                lightcurves=lightcurves,
+                file_path=file_path,
+        )
 
     gc.collect()
     tf.keras.backend.clear_session()
